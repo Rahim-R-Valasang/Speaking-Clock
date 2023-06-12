aaaf3d1c80f674f67fbcc2ec360df6a7089491f0
unknown <rahimvalasang@gmail.com>
2023-06-13 03:00:26 +0530

    Added test cases

--- src/test/java/com/example/clockproject/clock/ClockApplicationTests.java
+++ src/test/java/com/example/clockproject/clock/ClockApplicationTests.java
@@ -1,13 +1,48 @@
 package com.example.clockproject.clock;
 
+import com.example.clockproject.clock.controller.UserTimeConverterController;
+import com.example.clockproject.clock.service.TimeConverterService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 class ClockApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void testConvertUserTimeToWords_ValidTime_ReturnsConvertedTime() {
+		// Arrange
+		TimeConverterService timeConverterService = new TimeConverterService();
+		UserTimeConverterController userTimeConverterController = new UserTimeConverterController(timeConverterService);
+		LocalTime time = LocalTime.of(11, 25);
+		String expectedOutput = "It's eleven twenty-five";
+
+		// Act
+		ResponseEntity<String> response = userTimeConverterController.convertUserTimeToWords(time);
+
+		// Assert
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertEquals(expectedOutput, response.getBody());
 	}
+
+	@Test
+	public void testConvertUserTimeToWords_InvalidTime_ReturnsBadRequest() {
+		// Arrange
+		TimeConverterService timeConverterService = new TimeConverterService();
+		UserTimeConverterController userTimeConverterController = new UserTimeConverterController(timeConverterService);
+		LocalTime time = LocalTime.of(25, 70); // Invalid time
+
+		// Act & Assert
+		assertThrows(IllegalArgumentException.class, () -> {
+			userTimeConverterController.convertUserTimeToWords(time);
+		});
+	}
+
 
 }
